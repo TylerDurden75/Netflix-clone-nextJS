01758aa58f1ee990039f1e095a1f5593a9bf6e4f
TylerDurden75 <raytibo.j@gmail.com>
2022-04-12 15:30:16 +0200

    Redirection contionnally login user

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { magic } from "../lib/magic-client";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    getIsLoggedInAndRedirect();
+
+    return () => {
+      getIsLoggedInAndRedirect();
+    };
+  }, []);
+
+  const getIsLoggedInAndRedirect = async () => {
+    const isLoggedIn = await magic.user.isLoggedIn();
+    if (isLoggedIn) {
+      router.push("/");
+    } else {
+      router.push("/login");
+    }
+  };
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
