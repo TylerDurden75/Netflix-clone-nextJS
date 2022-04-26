84b31c70408bd97d108a4a0bd305bd1742b61266
TylerDurden75 <raytibo.j@gmail.com>
2022-04-26 17:09:37 +0200

    Requested from cookies

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,15 @@
 export default async function stats(req, res) {
   if (req.method === "POST") {
-    res.send({ msg: "it's ok" });
+    console.log({ cookies: req.cookies });
+    try {
+      if (!req.cookies.token) {
+        res.status(403).send({});
+      } else {
+        res.send({ msg: "it's ok" });
+      }
+    } catch (error) {
+      console.error("Something went wrong /stats", error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
   }
 }
