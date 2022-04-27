fbb5f5cf75efe09d3fc0663a463add10b73b8d7b
TylerDurden75 <raytibo.j@gmail.com>
2022-04-27 13:00:34 +0200

    Get stats request ok

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -18,11 +18,8 @@ export default async function stats(req, res) {
 
           const userId = decodedToken.issuer;
 
-          const doesStatsExist = await findVideoIdByUser(
-            token,
-            userId,
-            videoId
-          );
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const doesStatsExist = findVideo?.length > 0;
           if (doesStatsExist) {
             //update it
             const response = await updateStats(token, {
@@ -44,6 +41,34 @@ export default async function stats(req, res) {
           }
         }
         res.send({ msg: "it's ok", decodedToken, doesStatsExist });
+      }
+    } catch (error) {
+      console.error("Something went wrong /stats", error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
+  } else {
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        res.status(403).send({});
+      } else {
+        const { videoId } = req.body;
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+          const userId = decodedToken.issuer;
+
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const doesStatsExist = findVideo?.length > 0;
+          if (doesStatsExist) {
+            res.send(findVideo);
+          } else {
+            res.status(404);
+            res.send({ user: null, msg: "Video not found" });
+          }
+        } else {
+          res.status(500).send({ msg: "VideoId is required" });
+        }
       }
     } catch (error) {
       console.error("Something went wrong /stats", error);
